7a89dc69eea12b8af3592c57904d3ddd5d28eefd
Hariz Puad <dashstrike@users.noreply.github.com>
2026-09-05 19:32:22 +0800

    Load job icons across all pages

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 /* Compatibility bootstrap. Real configuration lives in ./js/config.js. */
 document.write('<script src="./js/config.js?v=20260905-1"><\/script>');
+document.write('<script src="./js/job-icons-global.js?v=20260905-1"><\/script>');
