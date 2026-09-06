6b82da97a23530b21556e84ab0c1ccbec3e9962d
Hariz Puad <dashstrike@users.noreply.github.com>
2026-09-06 11:42:05 +0800

    Refresh enlarged job icons

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 /* Compatibility bootstrap. Real configuration lives in ./js/config.js. */
 document.write('<script src="./js/config.js?v=20260905-1"><\/script>');
-document.write('<script src="./js/job-icons-global.js?v=20260905-1"><\/script>');
+document.write('<script src="./js/job-icons-global.js?v=20260906-2"><\/script>');
 document.write('<link rel="stylesheet" href="./css/party-stars.css?v=20260906-5">');
 
 document.addEventListener('DOMContentLoaded',()=>{
   if(document.querySelector('script[data-titania-party-stars]'))return;
   const script=document.createElement('script');
-  script.src='./js/party-stars.js?v=20260906-1';
+  script.src='./js/party-stars.js?v=20260906-2';
   script.setAttribute('data-titania-party-stars','1');
   document.body.appendChild(script);
 },{once:true});
