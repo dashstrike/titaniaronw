5e36c2334328984ac1910b2295797e06a5091f6d
Hariz Puad <dashstrike@users.noreply.github.com>
2026-09-06 11:56:26 +0800

    Load shared tooltips site-wide

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 /* Compatibility bootstrap. Real configuration lives in ./js/config.js. */
 document.write('<script src="./js/config.js?v=20260905-1"><\/script>');
 document.write('<script src="./js/job-icons-global.js?v=20260906-2"><\/script>');
-document.write('<link rel="stylesheet" href="./css/party-stars.css?v=20260906-6">');
+document.write('<link rel="stylesheet" href="./css/tooltips.css?v=20260906-1">');
+document.write('<script src="./js/tooltips-global.js?v=20260906-1"><\/script>');
+document.write('<link rel="stylesheet" href="./css/party-stars.css?v=20260906-7">');
 
 document.addEventListener('DOMContentLoaded',()=>{
   if(document.querySelector('script[data-titania-party-stars]'))return;
   const script=document.createElement('script');
-  script.src='./js/party-stars.js?v=20260906-3';
+  script.src='./js/party-stars.js?v=20260906-4';
   script.setAttribute('data-titania-party-stars','1');
   document.body.appendChild(script);
 },{once:true});
