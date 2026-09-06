680e5c954b0b8a6ab694a5ba4806366a2aa3a122
Hariz Puad <dashstrike@users.noreply.github.com>
2026-09-06 11:26:32 +0800

    Refresh Star alignment styles

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /* Compatibility bootstrap. Real configuration lives in ./js/config.js. */
 document.write('<script src="./js/config.js?v=20260905-1"><\/script>');
 document.write('<script src="./js/job-icons-global.js?v=20260905-1"><\/script>');
-document.write('<link rel="stylesheet" href="./css/party-stars.css?v=20260906-2">');
+document.write('<link rel="stylesheet" href="./css/party-stars.css?v=20260906-3">');
 
 document.addEventListener('DOMContentLoaded',()=>{
   if(document.querySelector('script[data-titania-party-stars]'))return;
