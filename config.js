496894b6fcf455a0bb240e71fd6e202cc9642109
Hariz Puad <dashstrike@users.noreply.github.com>
2026-09-07 15:02:40 +0800

    Refresh Runs navigation and picker behavior

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* Compatibility bootstrap. Real configuration lives in ./js/config.js. */
-document.write('<script src="./js/config.js?v=20260905-1"><\/script>');
+document.write('<script src="./js/config.js?v=20260907-2"><\/script>');
 document.write('<script src="./js/job-icons-global.js?v=20260906-3"><\/script>');
 document.write('<link rel="stylesheet" href="./css/crowns.css?v=20260906-2">');
 document.write('<link rel="stylesheet" href="./css/tooltips.css?v=20260906-1">');
@@ -7,7 +7,7 @@ document.write('<script src="./js/tooltips-global.js?v=20260906-2"><\/script>');
 document.write('<link rel="stylesheet" href="./css/party-stars.css?v=20260906-8">');
 document.write('<script src="./js/member-profile-enhancements.js?v=20260906-2"><\/script>');
 document.write('<script src="./js/user-management-link.js?v=20260906-1"><\/script>');
-document.write('<script src="./js/guild-runs-link.js?v=20260907-1"><\/script>');
+document.write('<script src="./js/guild-runs-link.js?v=20260907-2"><\/script>');
 document.write('<script src="./js/organizer-role.js?v=20260907-1"><\/script>');
 
 document.addEventListener('DOMContentLoaded',()=>{
