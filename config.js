2b65474affff3b34a3231ff537c1a44237b0df8d
Hariz Puad <dashstrike@users.noreply.github.com>
2026-09-10 21:12:01 +0800

    Refresh page heading icons

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ document.write('<script src="./js/tooltips-global.js?v=20260906-2"><\/script>');
 document.write('<link rel="stylesheet" href="./css/party-stars.css?v=20260906-8">');
 document.write('<script src="./js/member-profile-enhancements.js?v=20260906-2"><\/script>');
 document.write('<script src="./js/user-management-link.js?v=20260906-1"><\/script>');
-document.write('<script src="./js/guild-runs-link.js?v=20260908-2"><\/script>');
+document.write('<script src="./js/guild-runs-link.js?v=20260910-1"><\/script>');
 document.write('<script src="./js/organizer-role.js?v=20260907-1"><\/script>');
 
 document.addEventListener('DOMContentLoaded',()=>{
