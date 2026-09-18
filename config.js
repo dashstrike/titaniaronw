5b519a9897879e85929fba553f32a475ce887cce
Hariz Puad <dashstrike@users.noreply.github.com>
2026-09-18 10:02:19 +0800

    Bust root config cache for attendance icons

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* Compatibility bootstrap. Real configuration lives in ./js/config.js. */
 document.write('<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"><\/script>');
-document.write('<script src="./js/config.js?v=20260911-att-layout-2"><\/script>');
+document.write('<script src="./js/config.js?v=20260918-attendance-fa-2"><\/script>');
 document.write('<script src="./js/job-icons-global.js?v=20260908-2"><\/script>');
 document.write('<link rel="stylesheet" href="./css/crowns.css?v=20260906-2">');
 document.write('<link rel="stylesheet" href="./css/tooltips.css?v=20260906-1">');
@@ -12,7 +12,7 @@ document.write('<script src="./js/member-csv-import-link.js?v=20260912-1"><\/scr
 document.write('<script src="./js/user-management-link.js?v=20260906-1"><\/script>');
 document.write('<script src="./js/guild-runs-link.js?v=20260910-fa-1"><\/script>');
 document.write('<script src="./js/organizer-role.js?v=20260907-1"><\/script>');
-document.write('<link rel="stylesheet" href="./css/attendance-page.css?v=20260911-layout-1">');
+document.write('<link rel="stylesheet" href="./css/attendance-page.css?v=20260918-attendance-afk-1">');
 document.write('<script src="./js/attendance-delete.js?v=20260911-2"><\/script>');
 document.write('<script src="./js/attendance-layout.js?v=20260911-2"><\/script>');
 document.write('<script src="./js/party-pool.js?v=20260911-4"><\/script>');
